a0b6e42968aa5a2f8f34ff932b06121af8758ba8
Ravinder219 <reddyravi219@gmail.com>
2025-07-04 16:36:40 +0530

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,8 +1,16 @@
 # app.py
 # This is a test commit
+
 def add(a, b):
     return a + b
+
+def multiply(a, b):
+    return a * b
 
 def test_add():
     assert add(1, 8) == 9
     assert add(1, -1) == 0
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+    assert multiply(-1, 5) == -5
